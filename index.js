aeb17bdb516b13afa9c6381c4aab4af50be41a5d
Ragura <ragura@gmail.com>
2019-02-05 18:14:09 +0100

    Commentaar toegevoegd

--- index.js
+++ index.js
@@ -1,14 +1,30 @@
+// require() is het equivalent van import() voor Node applicaties
+// Express is het server framework
 const express = require("express");
+
+// Cross Origin Resource Sharing (CORS) zorgt ervoor dat een domein
+// aanvragen kan doen aan andere domeinen
 const cors = require("cors");
 
+// Maak een nieuwe express app 
 const app = express();
 
+// Een server draait op een adres en een poort.
+// Laat de poort uit de Node environment variable lezen
+// als ze niet bestaat, neem een standaardwaarde
 const port = process.env.PORT || 3333;
 
+// Require een eigen .js bestand dat routergegevens exporteert
 const producten = require('./routes/producten');
 
+// de use() methode van een express app registreert middleware.
+// Middleware zijn functies die worden uitgevoerd bij elke request aan de server
+// Er kunnen meerdere middleware functies geregistreerd worden
+// Ze worden uitgevoerd in volgorde van registratie
 app.use(cors());
 
+// de get() methode registreert een route met een bijhorende afhandelfunctie (handler)
+// zulke handler functie neemt als parameters minstens req en res aan (de request en de response)
 app.get("/", (req, res) => {
     // status zet de HTTP status code van de response header
     res.status(403).send("Gebruik de API!");
@@ -18,7 +34,7 @@ app.get("/", (req, res) => {
 // voor een bepaald pad, in dit geval het pad /producten
 app.use('/producten', producten);
 
-// Fallback, moet laatst in de reeks komen!
+// Fallback voor alle andere mogelijke adressen, moet laatst in de reeks komen!
 app.get("*", (req, res) => {
     // sendStatus stuurt een HTTP status code zonder boodschap
     res.sendStatus(404);
--- routes/producten.js
+++ routes/producten.js
@@ -1,12 +1,20 @@
+// Hoewel dit bestand los staat van het starten van de server,
+// hebben we onderdelen van het express pakket nodig om een router te maken
 const express = require('express');
 
+// Instantieer een nieuwe router
 const router = express.Router();
 
+// Ook andere lokale bestanden kunnen worden geïmporteerd met require()
 const { producten, categories } = require('../lijst_producten.js');
 
-// GET route
+// GET route voor de root '/'
+// Let op: dit "root" pad is niet hetzelfde als de root van de ganse app, enkel van deze router
 router.get('/', (req, res) => {
+    // De send() methode kan allerlei data doorsturen, in dit geval een json object
     res.send(producten);
 });
 
+// Om in Node objecten, functies, variabelen, ... te exporteren maak je gebruik van
+// module.exports
 module.exports = router;
